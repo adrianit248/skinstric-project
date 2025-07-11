b243a46802fa2e41fb5574946743d88136d3f530
adrianit248 <adrianit248@gmail.com>
2025-07-10 21:30:50 -0400

    working on transitions

--- src/pages/Intro.js
+++ src/pages/Intro.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { MdPlayArrow } from "react-icons/md"
+
 
 const Intro = () => {
   return (
@@ -6,7 +8,7 @@ const Intro = () => {
         <div className='main-container'>
             <div className='skinstric-header'>
                 <div className='skinstric-namestate'>
-                    <div className='skinstric-name'>Skinstric</div>
+                    <div className='skinstric-name'>sKINsTRIC</div>
                     <div className='skinstric-state'>{'[ INTRO ]'}</div>
                 </div>
                 <div className='header-button'>
@@ -15,9 +17,22 @@ const Intro = () => {
             </div>
             <div className='skinstric-body'>
                 <div className='left-holder'>
-                    <div className='intro-box left-side'></div>
+                    <div className='left-message-holder'>
+                        <div className='discover-ai-button discover-ai-button-left'>
+                            <MdPlayArrow className='arrow-icon rotated-90' />
+                        </div>
+                        <div className='discover-ai-text-left'>DISCOVER A.I.</div>
+                    </div>
+                    <div className='intro-box left-side'>
+                    </div>
                 </div>
                 <div className='right-holder'>
+                    <div className='right-message-holder'>
+                        <div className='discover-ai-text-right'>TAKE TEST</div>
+                        <div className='discover-ai-button discover-ai-button-right'>
+                            <MdPlayArrow className='arrow-icon rotated-none' />
+                        </div>
+                    </div>
                     <div className='intro-box right-side'></div>
                 </div>
                 <div className='middle-header'>
